5d18ae92733fe6a7f452b0b35df1e68dc2b7043f
DaXterGD <faniel5431@gmail.com>
2022-03-11 20:36:14 -0500

    [ADD] Project setup

---
+++ src/routes/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "@styles/index.css";
+
+const App = () => {
+  return <h1>Hola esta</h1>;
+};
+
+export default App;
